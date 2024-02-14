2b8d3e79f29443dcf19fe8d68a3f5b2100ed8d1b
ALauraFM <analaurafmendonca@gmail.com>
2024-02-14 16:19:02 -0300

    script update

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Check if Python is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python3 is not installed. Please install Python3 and try again."
+    exit 1
+fi
+
+# Install required Python packages
+pip install opencv-python pyzbar
+
+#Install pyrealsense2
+pip install pyrealsense2
+
+# Run the Python script
+python3 barcode_reader.py  
+
+
+# Exit the script
+exit 0
